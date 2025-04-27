d151304b116859cd653148202643bc79cf7f590f
nashahaha <ignacia.galaz@ug.uchile.cl>
2025-04-27 16:16:01 -0400

    Agrega implementaciones exit y enter ver1

--- T3/rwlock.c
+++ T3/rwlock.c
@@ -17,13 +17,13 @@ typedef enum {READER, WRITER} Kind;
 
 typedef struct {
   int ready;
-  Kind kind;
   pthread_cond_t w;
 } Request;
 
 pthread_mutex_t m = PTHREAD_MUTEX_INITIALIZER;
 int readers = 0, writing = 0;
-Queue *q;
+Queue *q_writer, *q_reader;
+
 
 RWLock *makeRWLock() {
   //...
@@ -35,7 +35,11 @@ void destroyRWLock(RWLock *rwl) {
 
 void enterRead(RWLock *rwl) {
   pthread_mutex_lock(&m);
-
+  
+  if(!writing && emptyQueue(q_writer)) // Se suma un lector (puede entrar) si no hay escritores trabajando o en espera 
+    readers++; //agregamos un reader
+  else
+    enqueue(READER);
   
   pthread_mutex_unlock(&m);
 }
@@ -43,6 +47,9 @@ void enterRead(RWLock *rwl) {
 void exitRead(RWLock *rwl) {
   pthread_mutex_lock(&m);
 
+  readers--;
+  if(readers==0 && !emptyQueue(q_writer)) // no quedan lectores trabajando y hay escritores en espera
+    wakeup(WRITER);
 
   pthread_mutex_unlock(&m);
 }
@@ -51,7 +58,7 @@ void enterWrite(RWLock *rwl) {
   pthread_mutex_lock(&m);
 
   if(readers==0 && !writing) // marca un escritor (puede entrar) solo si no hay lectores ni escritores usando el mutex
-    writing = 1;
+    writing = 1; //marca un escritor
   else // si no queda en espera
     enqueue(WRITER);
   
@@ -61,6 +68,49 @@ void enterWrite(RWLock *rwl) {
 void exitWrite(RWLock *rwl) {
   pthread_mutex_lock(&m);
   writing = 0; //declara que nadie está escribiendo
-  //WAKEUP -> lanza un signal a quien corresponda
+  if(!emptyQueue(q_reader)) // hay solicitudes de lectores
+    wakeup(READER);
+  else if (emptyQueue(q_reader) && !emptyQueue(q_writer)) // no hay lectores en espera y hay escritores en espera
+    wakeup(WRITER);
+  // WAKEUP -> lanza un signal a quien corresponda
+  // si hay solicitudes de lectores se aceptan todas
+  // si no hay lectores pendientes pero hay solicitudes de escritores, se acepta al escritor que lleve más tiempo esperando
   pthread_mutex_unlock(&m);
+}
+
+void enqueue(Kind kind){
+  Request req = {0, PTHREAD_COND_INITIALIZER};
+
+  if(kind==WRITER) // si es escritor espera en la cola de escritores
+    put(q_writer, &req);
+  else // es un lector
+    put(q_reader, &req);
+  
+  while(!req.ready) //mientras el request no esté listo, espera
+    pthread_cond_wait(&req.w, &m);
+}
+
+void wakeup(Kind kind){
+  if(kind==WRITER){
+    Request *r = get(q_writer);  
+    if(r==NULL)
+      return;
+    
+    writing = 1;
+    r->ready = 1; //para hacer verdadero el while
+    pthread_cond_signal(&r->w); //despertamos la condición que corresponda
+  
+  } else { // si el tipo es lector
+    
+    Request *r = get(q_reader);
+    if(r==NULL)
+      return;
+
+    do { //entran lectores consecutivos
+      r->ready = 1;
+      pthread_cond_signal(&r->w);
+      get(q_reader); //obtengo el siguiente
+    } while (r!=NULL);
+  }
+
 }
