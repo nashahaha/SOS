f1614e12e05baac579d884d6c79f155b793c7ecb
nashahaha <ignacia.galaz@ug.uchile.cl>
2025-05-01 15:50:17 -0400

    Upload T4 files

---
+++ T4/subasta.c
@@ -0,0 +1,22 @@
+#include <nthread-impl.h>
+#include "subasta.h"
+
+// =============================================================
+// Implemente a partir de aca el tipo Subasta, puede agregar
+// funciones, estructuras o variables que considere necesarias
+// =============================================================
+
+struct subasta{
+};
+
+nSubasta nNuevaSubasta(int n){
+}
+
+int nOfrecer(nSubasta s, double oferta){
+}
+
+double nAdjudicar(nSubasta s,int *punid ){
+}
+
+void nDestruirSubasta(nSubasta s){
+}
